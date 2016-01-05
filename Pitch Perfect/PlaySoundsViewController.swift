883168dec77bc41159d90a36dceae0d0a7de0096
Stephan Koenig <stephan.koenig@gmail.com>
2016-01-05 01:09:51 +0100

    feat: Add button and file for playback, implement playback

--- Pitch Perfect/PlaySoundsViewController.swift
+++ Pitch Perfect/PlaySoundsViewController.swift
@@ -7,13 +7,23 @@
 //
 
 import UIKit
+import AVFoundation
 
 class PlaySoundsViewController: UIViewController {
 
+    var audioPlayer: AVAudioPlayer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
+        if let filePath = NSBundle.mainBundle().pathForResource("movie_quote", ofType: "mp3") {
+            let filePathUrl = NSURL.fileURLWithPath(filePath)
+            audioPlayer = try! AVAudioPlayer(contentsOfURL: filePathUrl)
+            audioPlayer.enableRate = true
+        } else {
+            print("The filePath is empty.")
+        }
+
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,6 +31,11 @@ class PlaySoundsViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    @IBAction func playSlowAudio(sender: UIButton) {
+        audioPlayer.stop()
+        audioPlayer.rate = 0.5
+        audioPlayer.play()
+    }
 
     /*
     // MARK: - Navigation
